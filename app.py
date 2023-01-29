82706a6b9dc8c89a9588ad86ccb57d69da560532
lewi0332 <derrickjameslewis@gmail.com>
2023-01-29 10:17:19 -0600

    Initial App.py dev

--- app.py
+++ app.py
@@ -1,50 +1,66 @@
-from flask import Flask, session, jsonify, request
-import pandas as pd
-import numpy as np
-import pickle
-import create_prediction_model
-import diagnosis 
-import predict_exited_from_saved_model
 import json
 import os
+from flask import Flask, session, jsonify, request
+import pandas as pd
+# import numpy as np
+# import pickle
+# import create_prediction_model
+from diagnostics import model_predictions, dataframe_summary
+from diagnostics import execution_time, missing_data, outdated_packages_list
+# import predict_exited_from_saved_model
 
 
-
-######################Set up variables for use in our script
+# Set up variables for use in our script
 app = Flask(__name__)
-app.secret_key = '1652d576-484a-49fd-913a-6879acfa6ba4'
+# app.secret_key = '1652d576-484a-49fd-913a-6879acfa6ba4'
 
-with open('config.json','r') as f:
-    config = json.load(f) 
+with open('config.json', 'r', encoding='utf8') as f:
+    config = json.load(f)
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
+dataset_csv_path = os.path.join(config['output_folder_path'])
+model_path = os.path.join(config['prod_deployment_path'])
 
 prediction_model = None
 
 
-#######################Prediction Endpoint
-@app.route("/prediction", methods=['POST','OPTIONS'])
-def predict():        
-    #call the prediction function you created in Step 3
-    return #add return value for prediction outputs
+@app.route("/prediction", methods=['POST', 'OPTIONS'])
+def predict() -> str:
+    """
+    Calls the prediction function on the data in the config file
+    under 'output_folder_path'
+    """
+    filepath = request.args.get('filepath')
+    dff = pd.read_csv(filepath)
+    preds = model_predictions(dff, model_path)
+    return str(preds)
 
-#######################Scoring Endpoint
+# Scoring Endpoint
 @app.route("/scoring", methods=['GET','OPTIONS'])
-def stats():        
-    #check the score of the deployed model
-    return #add return value (a single F1 score number)
+def score():        
+    """heck the score of the deployed model"""
+    with open(dataset_csv_path + '/latestscore.txt', 'r', encoding='utf8'
+              ) as f1:
+        latestscore = f1.read()
+    return latestscore
 
-#######################Summary Statistics Endpoint
-@app.route("/summarystats", methods=['GET','OPTIONS'])
-def stats():        
-    #check means, medians, and modes for each column
-    return #return a list of all calculated summary statistics
+# Summary Statistics Endpoint
+@app.route("/summarystats", methods=['GET', 'OPTIONS'])
+def summary():
+    sumamry_dict = dataframe_summary(dataset_csv_path)
+    return sumamry_dict
 
-#######################Diagnostics Endpoint
-@app.route("/diagnostics", methods=['GET','OPTIONS'])
-def stats():        
-    #check timing and percent NA values
-    return #add return value for all diagnostics
 
-if __name__ == "__main__":    
+@app.route("/diagnostics", methods=['GET', 'OPTIONS'])
+def diagnose():
+    """
+    Calls the remaingin diagnostics functions and returns the results
+    """
+    timings = execution_time()
+    missing = missing_data(dataset_csv_path)
+    outdated = outdated_packages_list()
+    dianostics = {'timings': timings, 'missing': missing, 'outdated': outdated}
+    return dianostics
+
+
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000, debug=True, threaded=True)
